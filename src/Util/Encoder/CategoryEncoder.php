5ed871837d041c2fa36d9cfbacb14672b5e1787f
Bziluch <bziluch@gmail.com>
2024-01-24 00:17:00 +0100

    feat: categories - added universal encoder

---
+++ src/Util/Encoder/CategoryEncoder.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Util\Encoder;
+
+use App\Entity\Category;
+
+class CategoryEncoder extends AbstractEncoder
+{
+    public function __construct(
+        private readonly Category $category
+    ){}
+
+    protected function getKey(): string
+    {
+        return $this->category->getEncodeKey().Category::$KEY;
+    }
+
+    protected function getData(): ?string
+    {
+        return $this->category->getName();
+    }
+
+    protected function setData(string $data): void
+    {
+        $this->category->setName($data);
+    }
+
+    protected function getFlags(): ?string
+    {
+        return $this->category->getFlags();
+    }
+
+    protected function setFlags(string $flags): void
+    {
+        $this->category->setFlags($flags);
+    }
+}
