69c51631c70da443351eb1a8346e12aad933412a
Bziluch <bziluch@gmail.com>
2024-01-24 22:34:03 +0100

    feat: categories - connected to encoder

--- src/Util/Encoder/CategoryEncoder.php
+++ src/Util/Encoder/CategoryEncoder.php
@@ -12,7 +12,7 @@ class CategoryEncoder extends AbstractEncoder
 
     protected function getKey(): string
     {
-        return $this->category->getEncodeKey().Category::$KEY;
+        return $this->category->getFullKey();
     }
 
     protected function getData(): ?string
