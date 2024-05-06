4b5ecad7c55c7c6cd6a18db99c31ce0bc48cd1af
Bziluch <bziluch@gmail.com>
2024-05-06 23:20:22 +0200

    feat: category list & form improvements

--- src/Repository/ThreadRepository.php
+++ src/Repository/ThreadRepository.php
@@ -28,9 +28,15 @@ class ThreadRepository extends ServiceEntityRepository
 
     public function getCategoryThreads(?Category $category)
     {
-        $qb = $this->createQueryBuilder('t')
-            ->andWhere('t.category = :category')
-            ->setParameter('category', $category);
+        $qb = $this->createQueryBuilder('t');
+        if ($category) {
+            $qb
+                ->andWhere('t.category = :category')
+                ->setParameter('category', $category);
+        } else {
+            $qb
+                ->andWhere('t.category IS NULL');
+        }
         if (!$this->security->getUser()) {
             $qb->andWhere('t.secure = 0 OR t.secure IS NULL');
         }
