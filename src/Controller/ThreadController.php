ae2f4eb746b1889fb2f3d65baa1a7a37ff2ad34f
Bziluch <bziluch@gmail.com>
2024-05-12 15:42:28 +0200

    feat: category & thread buttons and redirects

--- src/Controller/ThreadController.php
+++ src/Controller/ThreadController.php
@@ -75,6 +75,7 @@ class ThreadController extends AbstractController
             $entity->_onSave();
             $entityManager->persist($entity);
             $entityManager->flush();
+            return $this->redirectToRoute('app_thread_view', ['id' => $entity->getId()]);
         }
         return $this->render('thread/form.html.twig', [
             'form' => $form->createView(),
