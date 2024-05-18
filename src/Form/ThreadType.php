75085a6776bde74a18d4af8dd483fa10f610e8da
Bziluch <bziluch@gmail.com>
2024-05-18 13:19:23 +0200

    feat: categories & threads - improved security + additional redirects

--- src/Form/ThreadType.php
+++ src/Form/ThreadType.php
@@ -2,7 +2,11 @@
 
 namespace App\Form;
 
+use App\Entity\Category;
 use App\Entity\Thread;
+use App\Trait\UserProviderTrait;
+use Doctrine\ORM\EntityRepository;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
@@ -13,17 +17,49 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class ThreadType extends AbstractType
 {
+    use UserProviderTrait;
+
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
+        $user = $this->getUser();
+
         $builder
-            ->add('title', TextType::class)
-            ->add('secure', CheckboxType::class, [
-                'required' => false
-            ])
+            ->add('title', TextType::class);
+
+        if ($user) {
+            $builder
+                ->add('secure', CheckboxType::class, [
+                    'required' => false
+                ]);
+        }
+
+        if ($builder->getData()?->getId() > 0) {
+            $builder
+                ->add('category', EntityType::class, [
+                    'class' => Category::class,
+                    'required' => false,
+                    'placeholder' => '-- Select category --',
+                    'query_builder' => function (EntityRepository $er) use ($user) {
+                        $qb = $er->createQueryBuilder('c');
+                        if (null === $user) {
+                            $qb->andWhere('c.secure = 0 OR c.secure IS NULL');
+                        }
+                        return $qb;
+                    }
+                ]);
+        }
+
+        $builder
             ->add('save', SubmitType::class, [
-                'label' => 'Add thread',
+                'label' => 'Save',
                 'attr' => [
                     'class' => 'btn btn-outline-light'
+                ]
+            ])
+            ->add('saveAndReturn', SubmitType::class, [
+                'label' => 'Save and return',
+                'attr' => [
+                    'class' => 'btn btn-outline-secondary'
                 ]
             ])
         ;
